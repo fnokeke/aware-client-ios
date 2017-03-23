ad86169f930a9777ed376a6116a8edd0483071a4
Fabian Okeke <fnokeke@gmail.com>
2017-03-23 00:22:14 -0400

    vibration now following clock time

---
+++ AWARE/AWAREFramework/Sensors/Screen/Vibrate.h
@@ -0,0 +1,13 @@
+//
+//  Vibrate.h
+//  AWARE
+//
+//  Created by Fabian Okeke on 3/13/17.
+//  Copyright © 2017 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#ifndef Vibrate_h
+#define Vibrate_h
+
+
+#endif /* Vibrate_h */
---
+++ AWARE/AWAREFramework/Sensors/Screen/Vibrate.m
@@ -0,0 +1,9 @@
+//
+//  Vibrate.m
+//  AWARE
+//
+//  Created by Fabian Okeke on 3/13/17.
+//  Copyright © 2017 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
